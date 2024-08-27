3bb22f7071cb1fb0a4b67bd73b48b2217a2cc71e
Mohammed Abdelmoneim <71524062+Mohammed-Abdelmoneim@users.noreply.github.com>
2024-08-27 20:40:52 +0300

    Create print.py

---
+++ greedy_algorithm/fractional_knapsack_problem/print.py
@@ -0,0 +1,19 @@
+from my_knapSack import Knapsack, Item
+
+
+def print_items(bag):
+  # print the total value, current capacity, and items in the bag
+  print("----------------------------")
+  print("total value:", bag.total_value)
+  print("current capacity:", bag.current_capacity)
+  print("items:")
+  print("n\tv\tw")
+  for i, item in enumerate(bag.items):
+    print(f"{item.name}\t{item.value}\t{item.weight}")
+
+
+def print_array(items):
+  # print the name, value, weight, and ratio of each item in the array
+  print("n\tv\tw\tr")
+  for i, item in enumerate(items):
+    print(f"{item.name}\t{item.value}\t{item.weight}\t{item.ratio}")
